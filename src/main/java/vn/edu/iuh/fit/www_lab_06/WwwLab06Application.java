86c4c6b065f4868026f1bb423ca918ed21d24a38
Ngô Ngọc Vũ <ngongocvu668@gmail.com>
2023-11-16 18:57:52 +0700

    create lab

---
+++ src/main/java/vn/edu/iuh/fit/www_lab_06/WwwLab06Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLab06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwLab06Application.class, args);
+    }
+
+}
